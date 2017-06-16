89b1722ca14fb490e1c3d5acb6938d3b2eda7c1c
Jianwen <JZhang@esri.com>
2017-06-16 09:25:42 -0700

    06/16/2017

---
+++ PythonML/Classfication/test/KernalSVMWithScikit-learn.py
@@ -0,0 +1,27 @@
+import numpy as np
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+
+#plt.scatter(X_xor[y_xor==1, 0], X_xor[y_xor==1, 1],
+#            c='b', marker='x', label='1')
+#plt.scatter(X_xor[y_xor==-1, 0], X_xor[y_xor==-1, 1],
+#            c='r', marker='s', label='-1')
+#plt.ylim(-3.0)
+#plt.legend()
+#plt.show()
+
+import os
+import sys
+#script_path = os.getcwd() + '\\functions'
+sys.path.append(os.getcwd())
+#from plot_decision_regions import *
+from functions.plot_decision_regions import *
+from sklearn.svm import SVC
+
+svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
+plt.legend(loc='upper left')
+plt.show()
---
+++ PythonML/Classfication/test/PerceptronTestWithScikit-learn.py
@@ -0,0 +1,79 @@
+# Prepare dataset.
+from sklearn import datasets
+import numpy as np
+iris = datasets.load_iris()
+data = iris.data[:, [2, 3]]
+catagory = iris.target
+
+# Split the dataset into separate training and testing datasets.
+from sklearn.cross_validation import train_test_split
+# Parameter 'test_size' means to split data into 30 percent test data and 70 percent training data.
+data_train, data_test, catagory_train, catagory_test = train_test_split(data, catagory, test_size=0.3, random_state=0)
+
+# Standardize the dataset.
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+sc.fit(data_train)
+data_train_std = sc.transform(data_train)
+data_test_std = sc.transform(data_test)
+
+# Train with perceptron model.
+from sklearn.linear_model import Perceptron
+ppn_test = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+ppn_test.fit(data_train_std, catagory_train)
+catagory_predict = ppn_test.predict(data_test_std)
+
+# Calculate the classification accuracy.
+from sklearn.metrics import accuracy_score
+print('Accuracy: %.2f' % accuracy_score(catagory_test, catagory_predict))
+
+# Function: Plot the decision regions.
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+def plot_decision_regions(X, y, classifier, 
+                    test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                         np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot all samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+        
+    # highlight test samples
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]   
+        plt.scatter(X_test[:, 0], X_test[:, 1], c='', 
+                alpha=1.0, linewidths=1, marker='o', 
+                s=55, label='test set')
+# Specify the indices of the samples that we want to mark on the resulting plots.
+data_combined_std = np.vstack((data_train_std, data_test_std))
+catagory_combined = np.hstack((catagory_train, catagory_test))
+plot_decision_regions(X=data_combined_std, y=catagory_combined, classifier=ppn_test, test_idx=range(105,150))
+plt.xlabel('petal length [standardized]') 
+plt.ylabel('petal width [standardized]') 
+plt.legend(loc='upper left')
+plt.show()
+
+"""
+# Important: 
+As we can see in the resulting plot, 
+the three flower classes cannot be perfectly separated by a linear decision boundaries.
+#
+The perceptron algorithm never converges on datasets that aren't perfectly linearly separable.
+"""
